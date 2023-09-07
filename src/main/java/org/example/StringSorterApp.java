d0f600ad486f97e2180076517ac5d54c36e72cd7
Anatolii_Omelchenko <omelchenko20@gmail.com>
2023-09-07 16:38:09 +0300

    feature: added implementation of sortStrings methdod

--- src/main/java/org/example/StringSorterApp.java
+++ src/main/java/org/example/StringSorterApp.java
@@ -1,10 +1,26 @@
 package org.example;
 
-/**
- * Hello world!
- */
+import java.util.List;
+import java.util.stream.Stream;
+
 public class StringSorterApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<String> unsortedStrings = List.of("apple", "banana", "grape", "avocado", "cherry");
+        List<String> sortedStrings = sortStrings(unsortedStrings, "a");
+
+        System.out.println("Unsorted: " + unsortedStrings);
+        System.out.println("Sorted: " + sortedStrings);
+    }
+
+    public static List<String> sortStrings(List<String> unsortedStrings, String exceptionChar) {
+        Stream<String> descSortedStrings = unsortedStrings.stream()
+                .filter(s -> s.startsWith(exceptionChar))
+                .sorted((s1, s2) -> s2.compareToIgnoreCase(s1));
+
+        Stream<String> ascSortedStrings = unsortedStrings.stream()
+                .filter(s -> !s.startsWith(exceptionChar))
+                .sorted();
+
+        return Stream.concat(ascSortedStrings, descSortedStrings).toList();
     }
 }
